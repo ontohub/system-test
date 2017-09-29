4f47b6011f721895ca25e074c817b6cc8f689925
Eileen Bolloff <ebolloff@informatik.uni-bremen.de>
2017-09-29 14:18:36 +0200

    Adds broken logout scenario.

--- features/ontohub.feature
+++ features/ontohub.feature
@@ -3,7 +3,7 @@ Feature: Ontohub
   In order to test our complete Ontohub application
   We want to clone all relevant repos and then run the following scenarios
 
-  Scenario: Run the init script
+Scenario: Run the init script
   Then the following directories should exist:
     | ../../ontohub-frontend |
     | ../../ontohub-backend |
@@ -13,15 +13,21 @@ Feature: Ontohub
   When I run `curl --connect-timeout 30 localhost:3002`
   Then the exit status should be 0
 
-  Scenario: Successful rollback
+Scenario: Successful rollback
   When I add a user to the database
   Then the user should be there
   When I do a rollback
   Then the user shouldn't be there
 
-  @javascript
-  Scenario: Login
+@javascript
+Scenario: Login
   Given I visit the start page
   When I click on the 'Sign in' button
   And I enter my credentials and click the 'Sign in' button
   Then I should be logged in
+
+@javascript
+Scenario: Logout
+  Given I am logged in
+  When I click on 'Sign out'
+  Then I should be logged out
--- features/steps/ontohub_steps.rb
+++ features/steps/ontohub_steps.rb
@@ -43,110 +43,28 @@ end
 Then(/^I should be logged in$/) do
   find('.right.menu .ui.item.dropdown .react-gravatar').click
   expect(page).to have_content('SIGNED IN AS')
+  # expect(page).to have_content('Profile')
+  # expect(page).to have_content('Sign out')
+  # find('#global-menu-sign-out-button').click
+  #  expect(page).to have_content('Sign in')
 end
 
-
-
-
-
-
-
-
-
-
+# Steps belong to 'Logout' scenario
 
 Given(/^I am logged in$/) do
-  find('.top-bar-right a#sign_in_menu_link').click
-  within('form#login') do
-    fill_in 'username', with: 'ada'
-    fill_in 'password', with: 'changeme'
-    click_button 'Sign in'
-  end
-  find('.top-bar-right a#user_menu_link').click
-  expect(page).to have_content('Signed in as')
-end
-
-When(/^I visit the repository creation page$/) do
-  find('.top-bar-right a#new-menu-link').click
-  click_link('New repository')
-  expect(page).to have_content('Create a new repository')
-end
-
-When(/^I create a repository$/) do
-  $repository_name = Faker::Name.title
-  within('fieldset#repository-new-name') do
-    fill_in 'Name', with: $repository_name
-  end
-  within('fieldset#repository-new-description') do
-    fill_in 'Description', with: Faker::Lorem.sentence
-  end
-  within('fieldset#repository-new-content-type') do
-    choose('Model')
-  end
-  within('fieldset#repository-new-access') do
-    choose('Public')
-  end
-  click_button 'Save'
-end
-
-Then(/^I should see the repository page$/) do
-  expect(page).to have_content("ada / #{$repository_name}")
-end
-
-
-Then(/^the repository should be visible in the repository overview page$/) do
-  visit('/search')
-  expect(page).to have_content("ada/#{$repository_name.parameterize}")
-end
-
-Then(/^the repository should be in the backend$/) do
   steps %{
-    When I run `curl -i -L -H "Content-Type: application/json" -H "Accept: application/vnd.api+json" http://localhost:#{$backend_port}/ada/#{$repository_name.parameterize}`
-    Then the exit status should be 0
-    And the output contains a line "HTTP/1.1 200 OK"
+    Given I visit the start page
+    When I click on the 'Sign in' button
+    And I enter my credentials and click the 'Sign in' button
+    Then I should be logged in
   }
 end
 
-Then(/^the output contains a line "([^"]*)"$/) do |arg|
-  expect(extract_text(unescape_text(last_command_started.output))).to match(/#{arg}/)
+When(/^I click on 'Sign out'$/) do
+  find('.right.menu .ui.item.dropdown .react-gravatar').click
+  find('#global-menu-sign-out-button').click
 end
 
-# Steps belong to 'edit repository' scenario
-
-Given(/^I am logged in and there is a repository I created$/) do
-  steps %{
-    Given I am logged in
-  }
-end
-
-When(/^I change the description of the repository$/) do
-  visit('/ada/repo0')
-  find('.top-route-header span').click
-  find('.input-group-field').set('Changed description of repo0')
-  find('.input-group-button button').click
-  wait_for_ajax #Wait for the changed description
-end
-
-Then(/^the changed repository should be visible in the repository overview page$/) do
-  visit('/search')
-  @description = 'Changed description of repo0'
-  expect(page).to have_content(@description)
-end
-
-Then(/^the changed repository should be in the backend$/) do
-  steps %{
-    When I run `curl -i -L -H "Content-Type: application/json" -H "Accept: application/vnd.api+json" http://localhost:#{$backend_port}/ada/repo0`
-    Then the exit status should be 0
-    And the output should contain "#{@description}"
-  }
-end
-
-Given(/^I visit a non\-existent page$/) do
-  visit('/non-existent')
-end
-
-Then(/^I should see the error page$/) do
-  expect(page).to have_content('Error The server responded with an error')
-  expect(page).to have_css('a', text: 'Go back')
-  expect(page).to have_link('Home')
+Then(/^I should be logged out$/) do
+  expect(page).to have_content('Sign in')
 end
